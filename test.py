7361f37bac64869c7b4a79fd8dcbe5b3ef03163b
rizkysan <rizky.rip46@gmail.com>
2024-11-30 10:48:26 +0700

    tes github action

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import threading
+import argparse
 
 def send_request(url, results):
     """Mengirimkan satu request ke URL dan mencatat waktu respon."""
@@ -65,16 +66,32 @@ def analyze_results(results):
     else:
         print("  Tidak ada data waktu respon yang tersedia.")
 
-if __name__ == "__main__":
-    # URL target
-    target_url = input("Masukkan URL target (contoh: https://example.com): ")
+# if __name__ == "__main__":
+#     # URL target
+#     target_url = input("Masukkan URL target (contoh: https://example.com): ")
 
-    # Jumlah permintaan total dan thread
-    total_requests = int(input("Masukkan jumlah total permintaan: "))
-    total_threads = int(input("Masukkan jumlah thread yang akan digunakan: "))
+#     # Jumlah permintaan total dan thread
+#     total_requests = int(input("Masukkan jumlah total permintaan: "))
+#     total_threads = int(input("Masukkan jumlah thread yang akan digunakan: "))
+
+#     # Melakukan load test
+#     results = load_test(target_url, total_requests, total_threads)
+
+#     # Menganalisis hasil
+#     analyze_results(results)
+
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Load testing script")
+    parser.add_argument("url", type=str, help="Target URL for load testing")
+    parser.add_argument("total_requests", type=int, help="Total number of requests")
+    parser.add_argument("total_threads", type=int, help="Number of threads to use")
+
+    args = parser.parse_args()
 
     # Melakukan load test
-    results = load_test(target_url, total_requests, total_threads)
+    results = load_test(args.url, args.total_requests, args.total_threads)
 
     # Menganalisis hasil
     analyze_results(results)
